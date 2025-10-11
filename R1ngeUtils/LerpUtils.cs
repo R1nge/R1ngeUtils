4dda3cdc4fe9d73b0ebc670bc19723ea5664f8b6
R1nge <r1ngegamedev@proton.me>
2025-10-11 09:42:37 +0300

    Create LerpUtils.cs

---
+++ R1ngeUtils/LerpUtils.cs
@@ -0,0 +1,34 @@
+public static class LerpUtils 
+{
+  public static IEnumerator<Vector3> LerpPosition3DCoroutine(Vector3 startPosition, Vector3 targetPosition, float duration) 
+  {
+    float time = 0;
+    Vector3 currentPosition;
+
+    while (time < duration) 
+    {
+      currentPosition = Vector3.Lerp(startPosition, targetPosition, time / duration);
+      time += Time.deltaTime;
+      yield return currentPosition;
+    }
+
+    currentPosition = targetPosition;
+    yield return currentPosition;
+  }
+
+  public static IEnumerator<Vector2> LerpPosition2DCoroutine(Vector2 startPosition, Vector2 targetPosition, float duration) 
+  {
+    float time = 0;
+    Vector2 currentPosition;
+
+    while (time < duration) 
+    {
+      currentPosition = Vector2.Lerp(startPosition, targetPosition, time / duration);
+      time += Time.deltaTime;
+      yield return currentPosition;
+    }
+
+    currentPosition = targetPosition;
+    yield return currentPosition;
+  }
+}
