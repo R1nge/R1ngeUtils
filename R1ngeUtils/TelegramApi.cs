df827e3156c31f80c852614230855321e8a6f88d
R1nge <r1ngegamedev@proton.me>
2025-10-13 00:44:26 +0300

    Create TelegramApi.cs

---
+++ R1ngeUtils/TelegramApi.cs
@@ -0,0 +1,20 @@
+namespace R1ngeUtils
+{
+public class TelegramApi
+    {
+        [DllImport("__Internal")]
+        public static extern string GetUserInfo();
+
+        [DllImport("__Internal")]
+        public static extern int GetSafeAreaTop();
+        
+        [DllImport("__Internal")]
+        public static extern int GetSafeAreaBottom();
+        
+        [DllImport("__Internal")]
+        public static extern int GetSafeAreaLeft();
+        
+        [DllImport("__Internal")]
+        public static extern int GetSafeAreaRight();
+    }
+}
